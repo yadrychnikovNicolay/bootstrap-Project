622de4c5419ae5c9cba24b6dafe7aeef305a61ee
YADRYCHNIKOV Nicolay <yadrychnikov.n@gmail.com>
2017-12-07 16:45:27 +0100

    finished, kind of

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ $('#myList a').on('click', function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+
+$(document).ready(function(){
+    $('#characterLeft').text('280 characters left');
+    $('#message').keydown(function () {
+        var max = 280;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit').addClass('red');
+            $('#submit').attr('disabled', true).removeClass('btn-primary').addClass('btn-danger');
+        }
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left').removeClass('red');
+            $('#submit').attr('disabled', false).addClass('btn-primary').removeClass('btn-danger') ;
+        }
+    });
+});
